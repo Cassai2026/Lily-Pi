9a4c053304b7677b2eaf312d1c0f2766e9e3f6a9
Cassai2026 <paul.cassidy2@ou.ac.uk>
2026-04-20 22:11:12 +0100

    FORGE: Phase 1 (101-104) - State, Metaphor, Query, Context.

--- lil-pi/game_engine/context_engine.py
+++ lil-pi/game_engine/context_engine.py
@@ -2,5 +2,6 @@
     def __init__(self):
         self.active_objects = []
     def ingest_vision_data(self, detected_objects):
+        # Prioritize high-value assets for learning
         self.active_objects = [obj for obj in detected_objects if obj.get('priority', 0) > 5]
         return self.active_objects[0] if self.active_objects else "ENV_STATIC"
--- lil-pi/game_engine/scaffold_state.py
+++ lil-pi/game_engine/scaffold_state.py
@@ -5,6 +5,7 @@
         self.frustration_threshold = 75
 
     def update_state(self, biometrics):
+        # Shifts gears based on heart rate or focus scores
         if biometrics['heart_rate'] > self.frustration_threshold:
             self.current_mode = "ZEN_SHIELD"
         elif biometrics['focus_score'] < 40:
